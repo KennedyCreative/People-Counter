19b1be6ee936a75735d4880780c1de4093afe5ea
Postch <50191722+KennedyCreative@users.noreply.github.com>
2024-02-22 22:13:23 +0900

    Final Project This simple app counts how many people can be added to a space. You can set a maximum number of people you desire, after which the counter button is disabled. Additionally, the number cannot be less than zero. It utilizes Stack to overlay buttons on images.

---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: CounterPeople(),
+  ));
+}
+
+class CounterPeople extends StatefulWidget {
+  const CounterPeople({super.key});
+
+  @override
+  State<CounterPeople> createState() => _CounterPeopleState();
+}
+
+class _CounterPeopleState extends State<CounterPeople> {
+  Size get size => MediaQuery.of(context).size;
+
+  int peopleCount = 1;
+  int numberMaxPeople = 20;
+
+  bool get minorZeroIs => peopleCount <= 0;
+  bool get majorNumberPeopleIs => peopleCount >= numberMaxPeople;
+  bool get attentionManyPeople => peopleCount >= (numberMaxPeople - (numberMaxPeople * 0.20));
+
+  void oneMore() => setState(() => peopleCount++);
+  void oneLess() => setState(() => peopleCount--);
+
+  void oneMorePeople() => setState(() => numberMaxPeople++);
+  void oneLessPeople() => setState(() => numberMaxPeople--);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset(
+                'images/image.webp',
+                fit: BoxFit.cover,
+                height: size.height,
+                width: size.width,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Expanded(
+                            child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Maximun number of people',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        )),
+                        SizedBox(
+                          width: 120,
+                          child: Row(
+                            children: [
+                              IconButton(onPressed: oneMorePeople, icon: const Icon(Icons.arrow_drop_up)),
+                              Text(
+                                numberMaxPeople.toString(),
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                              IconButton(onPressed: numberMaxPeople <= 2 ? null : oneLessPeople, icon: const Icon(Icons.arrow_drop_down)),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                      margin: const EdgeInsets.symmetric(vertical: 80),
+                      color: attentionManyPeople ? Colors.redAccent : Colors.white70,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Text(
+                          peopleCount.toString(),
+                          style: const TextStyle(fontSize: 30),
+                        ),
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: minorZeroIs ? null : oneLess,
+                        child: const Text(
+                          '-1',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      const SizedBox(width: 30),
+                      ElevatedButton(
+                        onPressed: majorNumberPeopleIs ? null : oneMore,
+                        child: const Text(
+                          '+1',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
